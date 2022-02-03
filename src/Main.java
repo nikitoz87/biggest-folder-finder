940aef52a9a6d49a65e35fe86f53f2d60bbccd18
Nikita Filkov <nikitoz1987@gmail.com>
2022-02-03 23:04:26 +0200

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+
         String folderPath = "D:\\!ISO";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        //System.out.println(getFolderSize(file));
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " milliseconds");
 
 
-
+        /*
+        MyThread thread = new MyThread(1);
+        MyThread thread2 = new MyThread(2);
+        thread.start();
+        thread2.start();
+        */
         //System.out.println(System.getProperties().get("user.dir"));
 
     }
 
-    public static long getFolderSize (File folder) {
+    public static long getFolderSize (File folder)
+    {
         if (folder.isFile()) {
             return folder.length();
         }
