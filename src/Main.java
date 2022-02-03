74830f6066af075a2079494659af75476ac19c9a
Nikita Filkov <nikitoz1987@gmail.com>
2022-02-03 22:21:06 +0200

    dev

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderPath = "D:\\!ISO";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+
+
+        //System.out.println(System.getProperties().get("user.dir"));
+
+    }
+
+    public static long getFolderSize (File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files ) {
+            sum += getFolderSize(file);
+        }
+
+        return sum;
+
+    }
+
+
+}
